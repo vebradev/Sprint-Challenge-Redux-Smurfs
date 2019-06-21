d41566eeebda5d6d8a1a4958bdd191e2c7f5ab79
vebradev <justinas@vebra.dev>
2019-06-21 10:57:38 +0300

    Finish wiring redux & display results on screen.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,63 @@
-import React, { Component } from 'react';
-import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSmurfs } from "../actions/index";
+import { bindActionCreators } from "redux";
+import styled from "styled-components";
+import Smurf from "../components/Smurf";
+import "./App.css";
+
+const StyledH2 = styled.h2`
+  font-size: 25px;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+const StyledUl = styled.ul`
+  padding: 0;
+  margin: 0;
+`;
+
 class App extends Component {
+  componentDidMount = () => {
+    this.props.fetchSmurfs();
+    console.log(this.props);
+  };
+
   render() {
+    const { smurfs, fetchingSmurfs } = this.props;
+    if (fetchingSmurfs) {
+      return <div>Stay put, smurfs are on the way ...</div>;
+    }
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+      <div>
+        <StyledH2>SMURFS! 2.0 W/ Redux</StyledH2>
+        <StyledUl>
+          {smurfs.map(smurf => (
+            <Smurf smurf={smurf} key={smurf.id} />
+          ))}
+        </StyledUl>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs
+  };
+};
+
+const matchDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      fetchSmurfs
+    },
+    dispatch
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  matchDispatchToProps
+)(App);
---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledLi = styled.li`
+    display: block;
+    padding: 20px 20px;
+    margin: 0;
+    font-size: 18px;
+    text-align: center;
+    border-radius: 4px;
+    background-color: #8158fc;
+`;
+
+const Smurf = ({ smurf }) => {
+  return (
+    <StyledLi>
+      <span>{smurf.name}</span>
+      <span>{smurf.age}</span>
+      <span>{smurf.height}</span>
+    </StyledLi>
+  );
+};
+
+export default Smurf;
